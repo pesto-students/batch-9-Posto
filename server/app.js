554a7181434a06881db0eba8d580f7044ff0dddc
Navneet Sahota <navneet.sahota@outlook.com>
2019-08-14 04:42:32 +0530

    chore/Setup eslint and husky (#32)
    close #3

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,12 +17,13 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
