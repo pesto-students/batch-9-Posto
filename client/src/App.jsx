48514c5adc949c2f1e3ab46be930662f2666bee9
Navneet Sahota <navneet.sahota@outlook.com>
2019-08-13 22:03:34 +0530

    feat/Setup client-side routes
    close #25

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Login from './views/Login';
 
 function App() {
-  return <Login />;
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/login" />} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Login} />
+        <Route path="/new-post" component={Login} />
+        <Route path="/edit-post" component={Login} />
+        <Route path="/search" component={Login} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
