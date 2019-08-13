04500adada9fc8ef1d197f27143b8e773642b0bc
sumeet rohra <sumeetrohra1@gmail.com>
2019-08-13 06:35:48 +0530

    chore/Setup eslint and husky
    close#3
    
    Solve merge conflicts

--- server/app.js
+++ server/app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +23,28 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
+<<<<<<< HEAD
 app.use(function (req, res, next) {
+=======
+<<<<<<< HEAD
+app.use(function (req, res, next) {
+=======
+app.use((req, res, next) => {
+>>>>>>> chore/Setup eslint and husky
+>>>>>>> chore/Setup eslint and husky
   next(createError(404));
 });
 
 // error handler
+<<<<<<< HEAD
 app.use(function (err, req, res, next) {
+=======
+<<<<<<< HEAD
+app.use(function (err, req, res, next) {
+=======
+app.use((err, req, res) => {
+>>>>>>> chore/Setup eslint and husky
+>>>>>>> chore/Setup eslint and husky
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
