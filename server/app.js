2a30d5de371bbefedbdd804d9cde49669d486ff3
sumeet rohra <sumeetrohra1@gmail.com>
2019-08-13 06:35:48 +0530

    chore/Setup eslint and husky
    close#3
    
    Solve merge conflicts

---
+++ server/app.js
@@ -0,0 +1,49 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+<<<<<<< HEAD
+app.use(function (req, res, next) {
+=======
+app.use((req, res, next) => {
+>>>>>>> chore/Setup eslint and husky
+  next(createError(404));
+});
+
+// error handler
+<<<<<<< HEAD
+app.use(function (err, req, res, next) {
+=======
+app.use((err, req, res) => {
+>>>>>>> chore/Setup eslint and husky
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
