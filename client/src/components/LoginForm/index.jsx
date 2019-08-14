a603f94ce32e3b3e4d61aed03f22df67888b6d1b
Navneet Sahota <navneet.sahota@outlook.com>
2019-08-14 23:44:13 +0530

    feat/Add proptype validation to existing components
    close #45

--- client/src/components/LoginForm/index.jsx
+++ client/src/components/LoginForm/index.jsx
@@ -1,24 +1,26 @@
-import React from "react";
-import { Button, Form, Message, Segment } from "semantic-ui-react";
+import React from 'react';
+import {
+  Button, Form, Message, Segment,
+} from 'semantic-ui-react';
 
-import EmailInput from "../EmailInput";
-import PasswordInput from "../PasswordInput";
+import EmailInput from '../EmailInput';
+import PasswordInput from '../PasswordInput';
 
 const Login = () => (
-	<>
-		<Form size="large">
-			<Segment stacked>
-				<EmailInput />
-				<PasswordInput />
-				<Button color="teal" fluid size="large">
-					Login
-				</Button>
-			</Segment>
-		</Form>
-		<Message>
-			New to us? <a href="https://google.com">Sign Up</a>
-		</Message>
-	</>
+  <>
+    <Form size="large">
+      <Segment stacked>
+        <EmailInput />
+        <PasswordInput />
+        <Button color="teal" fluid size="large">
+          Login
+        </Button>
+      </Segment>
+    </Form>
+    <Message>
+      New to us? <a href="https://google.com">Sign Up</a>
+    </Message>
+  </>
 );
 
 export default Login;
--- client/src/elements/InputField/index.jsx
+++ client/src/elements/InputField/index.jsx
@@ -1,15 +1,31 @@
-import React from "react";
-import { Form } from "semantic-ui-react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form } from 'semantic-ui-react';
 
-const InputField = props => (
-	<Form.Input
-		fluid
-		icon={props.icon}
-		iconPosition={props.iconPosition}
-		placeholder={props.placeholder}
-		type={props.type}
-		required
-	/>
+const InputField = ({
+  icon, iconPosition, placeholder, type,
+}) => (
+  <Form.Input
+    fluid
+    icon={icon}
+    iconPosition={iconPosition}
+    placeholder={placeholder}
+    type={type}
+    required
+  />
 );
+
+InputField.propTypes = {
+  type: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+  iconPosition: PropTypes.string,
+  placeholder: PropTypes.string,
+};
+
+InputField.defaultProps = {
+  icon: undefined,
+  iconPosition: 'left',
+  placeholder: '',
+};
 
 export default InputField;
