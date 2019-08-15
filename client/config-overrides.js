0826abdeacc51c779fefdbfbbb8d201b6f613ee1
Navneet Sahota <navneet.sahota@outlook.com>
2019-08-16 04:07:40 +0530

    fix/Fix client build on netlify
    fix #59

---
+++ client/config-overrides.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const rewireYarnWorkspaces = require('react-app-rewire-yarn-workspaces');
+
+module.exports = function override(config, env) {
+  return rewireYarnWorkspaces(config, env);
+};
