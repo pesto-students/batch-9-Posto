9c0cdf529b29a6efb64ca538a84860861604e21f
Navneet Sahota <navneet.sahota@outlook.com>
2019-08-15 08:42:22 +0530

    chore/Setup test for client (#48)
    * chore/Setup test on for client
    
    close #39
    
    * Remove duplicate line in App.jsx

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Router><App /></Router>, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />);
 });
