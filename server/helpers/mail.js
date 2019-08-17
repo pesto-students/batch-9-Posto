9aeaf090b7fd43c9046bfe28627bde4abaf33ed4
avinash360 <avithakur86@gmail.com>
2019-08-18 04:09:06 +0530

    feat/ add sending mail functionality

---
+++ server/helpers/mail.js
@@ -0,0 +1,31 @@
+import nodemailer from 'nodemailer';
+import Joi from '@hapi/joi';
+import pug from 'pug';
+import path from 'path';
+import MailSchema from '../validations/mail';
+import joiOptions from '../validations/joiOptions';
+import config from '../config';
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: config.EMAIL,
+    pass: config.EMAIL_PASSWORD,
+  },
+});
+
+const sendMail = async function sendMail(type, mailOptions, variables) {
+  if (config.ENVIRONMENT === 'dev') {
+    return;
+  }
+  const inputs = { ...mailOptions, type };
+  await Joi.validate(inputs, MailSchema, joiOptions);
+  const filePath = path.resolve(__dirname, `../mailTemplates/${type}.pug`);
+  const pugToHtml = pug.renderFile(filePath, variables);
+  inputs.html = pugToHtml;
+  await transporter.sendMail(inputs);
+};
+
+export default {
+  sendMail,
+};
