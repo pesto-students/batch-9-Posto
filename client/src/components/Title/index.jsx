de6f301bc7f543927f171e652aff100a4be70d29
Navneet Sahota <navneet.sahota@outlook.com>
2019-08-19 06:32:33 +0530

    Refactor components

--- client/src/components/Title/index.jsx
+++ client/src/components/Title/index.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Header } from 'semantic-ui-react';
 
-const Title = ({ as, children }) => <Header color="teal" as={as}>{children}</Header>;
+const Title = ({ as, children, ...props }) => <Header as={as} {...props}>{children}</Header>;
 
 Title.propTypes = {
   as: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']).isRequired,
   children: PropTypes.string.isRequired,
+  color: PropTypes.string,
+};
+
+Title.defaultProps = {
+  color: 'teal',
 };
 
 export default Title;
