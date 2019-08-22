6c052811ff5d9a54c19135288b91b0eeab97dde6
avinash360 <avithakur86@gmail.com>
2019-08-22 16:51:07 +0530

    feat/ get categories list

---
+++ server/__tests__/controllers/categories.test.js
@@ -0,0 +1,38 @@
+import request from 'supertest';
+import addToDatabase from '../testUtils/addToDatabase';
+import testDbHelper from '../testUtils/testDbHelper';
+import app from '../../app';
+
+beforeAll(async () => {
+  await testDbHelper.createConnection();
+});
+
+afterAll(async () => {
+  await testDbHelper.closeConnection();
+});
+
+afterEach(async () => {
+  await testDbHelper.cleanup();
+});
+
+
+describe('Get Categories', () => {
+  test('should fetch a list of categories', async (done) => {
+    const categories = await addToDatabase.createCategories();
+    request(app)
+      .get('/categories')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect(async (response) => {
+        expect(response.body.success).toBe(true);
+        expect(response.body.message).toBe('Categories fetched');
+        expect(response.body.categories.length).toEqual(categories.length);
+      })
+      .end((error) => {
+        if (error) {
+          return done(error);
+        }
+        return done();
+      });
+  });
+});
--- server/__tests__/testUtils/addToDatabase.js
+++ server/__tests__/testUtils/addToDatabase.js
@@ -1,5 +1,6 @@
 import postModel from '../../models/Post';
 import userModel from '../../models/User';
+import categoryModel from '../../models/Category';
 
 async function createPosts() {
   const post1 = await postModel.create({
@@ -46,8 +47,63 @@ async function createUsers() {
   return { user1, user2 };
 }
 
+async function createCategories() {
+  const category1 = await categoryModel.create({
+    name: 'Art',
+  });
+
+  const category2 = await categoryModel.create({
+    name: 'Books',
+  });
+
+  const category3 = await categoryModel.create({
+    name: 'Food',
+  });
+
+  const category4 = await categoryModel.create({
+    name: 'Business',
+  });
+
+  const category5 = await categoryModel.create({
+    name: 'Movies',
+  });
+
+  const category6 = await categoryModel.create({
+    name: 'Fitness',
+  });
+
+  const category7 = await categoryModel.create({
+    name: 'Travel',
+  });
+
+  const category8 = await categoryModel.create({
+    name: 'Politics',
+  });
+
+  const category9 = await categoryModel.create({
+    name: 'News',
+  });
+
+  const category10 = await categoryModel.create({
+    name: 'Entertainment',
+  });
+
+  return [
+    category1,
+    category2,
+    category3,
+    category4,
+    category5,
+    category6,
+    category7,
+    category8,
+    category9,
+    category10,
+  ];
+}
 
 export default {
   createUsers,
   createPosts,
+  createCategories,
 };
