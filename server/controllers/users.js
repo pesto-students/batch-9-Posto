119f207ed39fdead6d7487801bf8b844a64cddef
avinash360 <avithakur86@gmail.com>
2019-08-23 22:23:42 +0530

    sub-feat/ get users api and tests

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,14 +1,17 @@
-import { ObjectID } from 'mongodb';
+import { ObjectId } from 'mongodb';
 import User from '../models/User';
 
 async function getUser(req, res) {
   try {
-    const userId = ObjectID(req.params.userId);
-    const user = await User.findById(userId);
-    if (user) {
-      return res.status(200).json({ success: true, message: 'user found', user });
+    const { userId } = req.params;
+    if (!ObjectId.isValid(userId)) {
+      return res.status(400).json({ success: false, message: 'Invalid user id value' });
     }
-    return res.status(200).json({ success: false, message: 'user not found', user });
+    const user = await User.findById(userId).select('name email profilePic gender DOB');
+    if (user) {
+      return res.status(200).json({ success: true, message: 'User found', user });
+    }
+    return res.status(200).json({ success: false, message: 'User not found' });
   } catch (error) {
     return res.json(400).json({ success: false, message: error.message });
   }
