a071e224aeda7ef69697ed9139933305e98808aa
avinash360 <avithakur86@gmail.com>
2019-08-24 10:35:11 +0530

    sub-feat/update user api

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,5 +1,9 @@
+import Joi from '@hapi/joi';
 import { ObjectId } from 'mongodb';
 import User from '../models/User';
+
+import UserSchema from '../validations/users';
+import joiOptions from '../validations/joiOptions';
 
 async function getUser(req, res) {
   try {
@@ -7,16 +11,39 @@ async function getUser(req, res) {
     if (!ObjectId.isValid(userId)) {
       return res.status(400).json({ success: false, message: 'Invalid user id value' });
     }
-    const user = await User.findById(userId).select('name email profilePic gender DOB');
+    const user = await User.findById(userId).select('name email profilePic gender DOB').lean();
     if (user) {
       return res.status(200).json({ success: true, message: 'User found', user });
     }
     return res.status(200).json({ success: false, message: 'User not found' });
   } catch (error) {
-    return res.json(400).json({ success: false, message: error.message });
+    return res.status(400).json({ success: false, message: error.message });
   }
 }
 
+const updateUser = async function updateUser(req, res) {
+  try {
+    const { userId } = req.params;
+    if (!ObjectId.isValid(userId)) {
+      return res.status(400).json({ success: false, message: 'Invalid user id value' });
+    }
+    const { body } = req;
+    await Joi.validate(body, UserSchema, joiOptions);
+    const updatedUser = await User.findByIdAndUpdate(userId, body, { new: true, select: 'name email profilePic gender DOB' }).lean();
+    if (updatedUser) {
+      return res.status(200).json({ success: true, message: 'User updated successfully', updatedUser });
+    }
+    return res.status(200).json({ success: false, message: 'User could not be updated' });
+  } catch (error) {
+    const errorResponse = { success: false, message: 'Could not update user', isJoi: !!error.isJoi };
+    if (error.isJoi) {
+      errorResponse.error = error.details;
+    }
+    return res.status(400).json(errorResponse);
+  }
+};
+
 module.exports = {
   getUser,
+  updateUser,
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,19 +3,9 @@ import UserController from '../controllers/users';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({ success: true, message: '' });
-});
-
 router.get('/:userId', UserController.getUser);
 
-router.post('/', (req, res) => {
-  res.status(200).json({ success: true, message: '' });
-});
-
-router.put('/:userId', (req, res) => {
-  res.status(200).json({ success: true, message: '' });
-});
+router.put('/:userId', UserController.updateUser);
 
 router.delete('/:userId', (req, res) => {
   res.status(200).json({ success: true, message: '' });
