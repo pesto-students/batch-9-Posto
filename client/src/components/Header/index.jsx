b97d2d18e65f579bcfd213a83922935d5850411c
sumeet rohra <sumeetrohra1@gmail.com>
2019-08-24 20:46:10 +0530

    fix/Make header responsive
    close #123

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
@@ -10,10 +10,25 @@ import SearchBox from '../../elements/SearchBox';
 import DropDown from '../../elements/DropDown';
 import ImageElement from '../../elements/ImageElement';
 
+import postoSm from '../../assets/postoSm.svg';
 import posto from '../../assets/posto.svg';
 import avatar from '../../assets/avatar.png';
 
 const Header = ({ history }) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const imageUrl = windowWidth >= 650 ? posto : postoSm;
+
+  const handleWindowResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  });
   const dropDownOptions = [
     { key: 1, text: 'Profile', value: 1 },
     { key: 2, text: 'Add Post', value: 2 },
@@ -26,11 +41,11 @@ const Header = ({ history }) => {
       <CenterContainer>
         <MenuItem>
           <ImageElement
-            src={posto}
+            src={imageUrl}
             alt="Posto Logo"
             style={{
               height: '30px',
-              width: '80px',
+              width: window.innerWidth >= 650 ? '80px' : '15px',
               cursor: 'pointer',
             }}
             onClick={() => history.push('/home')}
