5a8aa0245765848dabee69a5a016f899c774ae01
avinash360 <avithakur86@gmail.com>
2019-08-26 18:31:38 +0530

    reset password api

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { signIn, signUp, forgotPassword } from '../controllers/auth';
+import {
+  signIn, signUp, forgotPassword, resetPassword,
+} from '../controllers/auth';
 
 const router = express.Router();
 
 router.post('/signup', signUp);
 router.post('/signin', signIn);
 router.post('/forgot-password', forgotPassword);
+router.post('/reset-password', resetPassword);
 
 export default router;
