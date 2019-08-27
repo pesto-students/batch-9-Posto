f16d3c319e1a8afc42d05df967e7d3955133a5a5
sumeet rohra <sumeetrohra1@gmail.com>
2019-08-27 21:01:06 +0530

    fix/Make BlogList component reusable
    close #137

--- client/src/components/BlogList/index.jsx
+++ client/src/components/BlogList/index.jsx
@@ -1,29 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import axiosConfig from '../../config/axiosConfig';
 import BlogCard from '../BlogCard';
 import LoaderCentered from '../../elements/LoaderCentered/LoaderCenter';
 import { defaultContent, defaultImage } from '../../config/constants';
 
-export default function BlogList() {
-  const [blogs, setBlogs] = useState([]);
-
-  const fetchBlogs = async () => {
-    const params = {
-      limit: 10,
-      skip: 0,
-      orderBy: 'createdAt',
-      orderType: -1,
-    };
-    const response = await axios.get(`posts?limit=${params.limit}&skip=${params.skip}&orderby=${params.orderBy}&orderType=${params.orderType}`, axiosConfig);
-    setBlogs(response.data.posts);
-  };
-
-  useEffect(() => {
-    fetchBlogs();
-  }, []);
-
+export default function BlogList({ blogs }) {
   const conditionallyRenderBlogs = () => {
     if (blogs.length === 0) {
       return <LoaderCentered />;
@@ -44,3 +26,23 @@ export default function BlogList() {
     </div>
   );
 }
+
+BlogList.defaultProps = {
+  blogs: [],
+};
+
+BlogList.propTypes = {
+  blogs: PropTypes.arrayOf(
+    PropTypes.shape({
+      author: PropTypes.string,
+      category: PropTypes.string,
+      content: PropTypes.string,
+      public: PropTypes.bool,
+      published: PropTypes.bool,
+      title: PropTypes.string,
+      upvotes: PropTypes.array,
+      views: PropTypes.array,
+      _id: PropTypes.string,
+    }),
+  ),
+};
--- client/src/views/Home/index.jsx
+++ client/src/views/Home/index.jsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-import { Container } from 'semantic-ui-react';
+import axiosConfig from '../../config/axiosConfig';
+import CenteredContainer from '../../elements/CenterContainer';
 import Header from '../../components/Header';
 import CategoryScrollBar from '../../components/CategoryScrollBar';
 import BlogList from '../../components/BlogList';
 
-const HomePage = () => (
-  <>
-    <Header />
-    <CategoryScrollBar />
-    <Container style={{ paddingTop: '30px', paddingBottom: '50px' }}>
-      <h1
-        style={{ paddingBottom: '20px' }}
-      >Top 10 Blogs.
-      </h1>
-      <BlogList />
-    </Container>
-  </>
-);
+const HomePage = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchBlogs = async () => {
+    const params = {
+      limit: 10,
+      skip: 0,
+      orderBy: 'createdAt',
+      orderType: -1,
+    };
+    const response = await axios.get(`posts?limit=${params.limit}&skip=${params.skip}&orderby=${params.orderBy}&orderType=${params.orderType}`, axiosConfig);
+    setBlogs(response.data.posts);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <CategoryScrollBar />
+      <CenteredContainer style={{ paddingTop: '30px', paddingBottom: '50px' }}>
+        <h1
+          style={{ paddingBottom: '20px' }}
+        >Top 10 Blogs.
+        </h1>
+        <BlogList blogs={blogs} />
+      </CenteredContainer>
+    </>
+  );
+};
 
 export default HomePage;
