63487200568106f93ea85390fd70d1ab10b366b1
Navneet Sahota <navneet.sahota@outlook.com>
2019-08-31 21:47:04 +0530

    fix/New and Edit posts responsiveness
    close #199

--- client/src/components/PostMenu/index.jsx
+++ client/src/components/PostMenu/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { Menu, Icon } from 'semantic-ui-react';
 
 import MenuDropDown from '../../elements/MenuDropDown';
@@ -6,13 +6,27 @@ import GlobalContext from '../../context/GlobalContext';
 import {
   ACTIVE_TAB,
 } from '../../context/constants';
+import styles from './PostMenu.module.css';
 
 const PostMenu = () => {
   const { state, dispatch } = useContext(GlobalContext);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const handleWriteClick = () => dispatch({ type: ACTIVE_TAB, payload: 'write' });
   const handlePreviewClick = () => dispatch({ type: ACTIVE_TAB, payload: 'preview' });
   const handleHelpClick = () => dispatch({ type: ACTIVE_TAB, payload: 'help' });
+
+  const handleWindowResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  });
 
   return (
     <Menu
@@ -21,11 +35,23 @@ const PostMenu = () => {
         position: 'sticky', marginTop: '60px', zIndex: 4, top: '60px',
       }}
     >
-      <Menu.Item name="Write" active={state.activeTab === 'write'} onClick={handleWriteClick} />
-      <Menu.Item name="Preview" active={state.activeTab === 'preview'} onClick={handlePreviewClick} />
+      <Menu.Item className={styles.item} name="Write" active={state.activeTab === 'write'} onClick={handleWriteClick}>
+        {
+          windowWidth <= 650
+            ? <Icon circular inverted color="teal" name="write square" />
+            : null
+        }
+      </Menu.Item>
+      <Menu.Item className={styles.item} name="Preview" active={state.activeTab === 'preview'} onClick={handlePreviewClick}>
+        {
+          windowWidth <= 650
+            ? <Icon circular inverted color="teal" name="file text" />
+            : null
+        }
+      </Menu.Item>
       <Menu.Menu position="right">
         <MenuDropDown />
-        <Menu.Item active={state.activeTab === 'help'} onClick={handleHelpClick} position="right">
+        <Menu.Item className={styles.item} active={state.activeTab === 'help'} onClick={handleHelpClick} position="right">
           <Icon circular inverted color="teal" name="help" />
         </Menu.Item>
       </Menu.Menu>
