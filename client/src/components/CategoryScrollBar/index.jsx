1912dcfbef5357566b39b30f05faf184b424bc68
Navneet Sahota <navneet.sahota@outlook.com>
2019-08-31 23:33:33 +0530

    fix/Set active NavLink in CategoryScrollbar
    close #203

--- client/src/components/CategoryScrollBar/index.jsx
+++ client/src/components/CategoryScrollBar/index.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 
 import { getCategories } from '../../API';
 import styles from './CategoryScrollBar.module.css';
 
-const CategoryScrollBar = () => {
+const CategoryScrollBar = ({ selectedCategory }) => {
   const [categories, setCategories] = useState([]);
 
   // eslint-disable-next-line consistent-return
@@ -25,29 +26,39 @@ const CategoryScrollBar = () => {
   }, []);
 
   const renderCategories = () => (
-      <div className={styles.scrollmenu}>
-        {categories.map((category) => (
-          <Link
-            className={styles.linkStyle}
-            to={{
-              pathname: `/category/${category.text}`,
-              state: {
-                categoryId: category.key,
-              },
-            }}
-            key={category.key}
-          >
-            {category.text}
-          </Link>
-        ))}
-      </div>
-    );
+    <div className={styles.scrollmenu}>
+      {categories.map((category) => (
+        <NavLink
+          className={styles.linkStyle}
+          activeClassName={styles.active}
+          isActive={() => selectedCategory === category.key}
+          to={{
+            pathname: `/category/${category.text}`,
+            state: {
+              categoryId: category.key,
+            },
+          }}
+          key={category.key}
+        >
+          {category.text}
+        </NavLink>
+      ))}
+    </div>
+  );
 
   return (
     <>
       {renderCategories()}
     </>
   );
+};
+
+CategoryScrollBar.propTypes = {
+  selectedCategory: PropTypes.string,
+};
+
+CategoryScrollBar.defaultProps = {
+  selectedCategory: '',
 };
 
 export default CategoryScrollBar;
