197558f11ba080a3b3cac6f7c9bd3947c111c4fa
sumeet rohra <sumeetrohra1@gmail.com>
2019-09-01 15:21:14 +0530

    feat/Image upload firebase
    closes #185 #72

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import React, { useContext, useReducer } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import * as firebase from 'firebase';
 
 import Routes from './Routes';
 import reducer from './context/reducer';
 import GlobalContext from './context/GlobalContext';
+import config from './firebaseConfig';
 
 const App = () => {
+  try {
+    firebase.initializeApp(config);
+  } catch (err) { }
   const initialState = useContext(GlobalContext);
   initialState.user = JSON.parse(localStorage.getItem('user'));
   const [state, dispatch] = useReducer(reducer, initialState);
